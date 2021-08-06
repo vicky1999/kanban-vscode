815a938820ee93f17a63676b173ef1e5a845ac64
vicky1999 <ravivignesh2000@gmail.com>
2021-08-06 21:59:30 +0530

    webview created

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,9 @@ const path = require('path');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+	// Load KanbanBoard file
+	const KanbanBoard = require(context.extensionPath+"\\src\\kanbanBoard.ts");
 	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -43,6 +46,18 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 		});
 
+		//Load Kanban page
+		const panel = vscode.window.createWebviewPanel(
+			'kanbanBoard',
+			'Kanban Board',
+			vscode.ViewColumn.One,
+			{
+				enableScripts: true
+			}
+		);
+		// Load the html page
+		panel.webview.html = KanbanBoard.loadKanbanBoard();
+	
 
 	});
 
---
+++ src/kanbanBoard.ts
@@ -0,0 +1,14 @@
+const loadKanbanBoard = () => {
+    return `
+    <html>
+        <head>
+            
+        </head>
+        <body>
+            <h1>Kanban Board</h1>
+        </body>
+    </html>
+    `;
+};
+
+module.exports.loadKanbanBoard = loadKanbanBoard;
