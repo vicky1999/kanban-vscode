14136e51960b6ea26020e2d2ee49f19d3e69f071
vicky1999 <ravivignesh2000@gmail.com>
2021-08-06 21:35:24 +0530

    json file creation

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+const fs = require('fs');
+const path = require('path');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,17 +15,34 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('kanban-vscode.helloWorld', () => {
+
+	vscode.window.showInformationMessage('Extension Started!');
+	
+	let disposable = vscode.commands.registerCommand('kanban-vscode.main', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Extension Started!');
 		
 		if(vscode.workspace.rootPath === undefined) {
 			vscode.window.showErrorMessage("No Workspace selected!");
 			return;
 		}
 		vscode.window.showInformationMessage("Kanban Board for "+vscode.workspace.name+" is opened!");
-		
+
+		let data = `{\n\t"completed": [],\n\t"inProgress": [],\n\t"testing": [],\n\t"toDo": []\n}`;
+		let folderPath = vscode.workspace.rootPath+"\\.vscode";
+		let jsonPath = folderPath+"\\kanban.json";
+
+		// create kanban json file
+		if(!fs.existsSync(folderPath)) {
+			fs.mkdirSync(folderPath);
+		}
+
+		fs.writeFile(jsonPath,data,(err: any) => {
+			if(err) {
+				vscode.window.showErrorMessage("Something went Wrong!");
+			}
+		});
+
 
 	});
 
