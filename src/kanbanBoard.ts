caa0a10c56e5095e3e51807ffa71cd10b327d0a8
vicky1999 <ravivignesh2000@gmail.com>
2021-08-10 19:12:34 +0100

    delete task button added

--- src/kanbanBoard.ts
+++ src/kanbanBoard.ts
@@ -52,6 +52,10 @@ const loadKanbanBoard = (bootstrap,data,sortablejs) => {
                     });
                 }
 
+                function deleteTask() {
+                    console.log("Delete Task");
+                }
+
                 Sortable.create(todo,{
                     group: {
                         name: "kanban",
@@ -196,15 +200,16 @@ const loadKanbanBoard = (bootstrap,data,sortablejs) => {
                         for(let i=1;i<toChildren.length;i++) {
                             toArr.push({"name":toChildren[i].innerText});
                         }
-                        vscode.postMessage({
-                            command: 'changes',
-                            text: {
-                                fromId: fromId,
-                                toId: toId,
-                                from: fromArr,
-                                to: toArr
-                            }
-                        })
+                        console.log(toArr);
+                        // vscode.postMessage({
+                        //     command: 'changes',
+                        //     text: {
+                        //         fromId: fromId,
+                        //         toId: toId,
+                        //         from: fromArr,
+                        //         to: toArr
+                        //     }
+                        // })
                     },
                     animation: 150,
                     ghostClass: 'ghost'
@@ -224,7 +229,15 @@ const displayData = (data,key,name) => {
                 <ul id="${key}" class="list-group list-group-flush">
             `;
     for(let i=0;i<data.length;i++) {
-        res+=`<li class="list-group-item">${data[i].name}</li>`;
+        res+=`
+        <li class="list-group-item">
+            ${data[i].name}
+            <button type="button" class="btn btn-outline-danger" onclick="deleteTask();" style="float: right;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                </svg>
+            </button>
+        </li>`;
     }
     res+=`</ul></div>`;
 
